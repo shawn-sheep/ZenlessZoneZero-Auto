3017e425cc89fe3cd5165e038405fdfe6d5e3f7d
Shawn_Sheep <shawnxu_0227@163.com>
2024-10-16 20:32:42 +0800

    Revert "Merge branch 'sMythicalBird:master' into master"
    This reverts commit 4a4a08d1c9614844c1fd9967038465269e1089e9, reversing
    changes made to 33a1e0cd4b8bd5e4d1f3221dceea2c16e883f9e3.

--- start_task.py
+++ start_task.py
@@ -7,7 +7,7 @@
 from threading import Thread
 from pynput.keyboard import Key, Listener
 
-from utils.task import task_zero, task_money, task_code, task_fight, task_daily
+from utils.task import task_zero, task_money, task_code, task_fight
 
 
 # 测试更新情况
@@ -79,32 +79,6 @@ def redemption_code():
     # 任务开始
     task_code.run()
 
-# 自动化日常任务
-def daily():
-    # 监听运行状态
-    key_thread = Thread(target=key_event, args=(task_daily,))
-    key_thread.start()
-
-    # 导入任务
-    import event_handling.daily
-
-    # 任务开始
-    task_daily.run()
-
-
-def new_account():
-    # 监听运行状态
-    key_thread = Thread(target=key_event, args=(task_daily,))
-    key_thread.start()
-
-    # 导入任务
-    import event_handling.test
-
-    # 任务开始
-    task_daily.run()
-
-
-
 
 def start_task(action):
     if action == "zero":
@@ -116,11 +90,3 @@ def start_task(action):
     elif action == "fight":
         print("start fight task")
         fight_task()
-    elif action == "daily":
-        print("start daily task")
-        daily()
-
-
-if __name__ == '__main__':
-    start_task('daily')
-    # new_account()
